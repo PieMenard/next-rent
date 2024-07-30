794bc63618a7d572210f820c7eb8c4bf733c6f61
PieMenard <110061246+PieMenard@users.noreply.github.com>
2024-07-30 15:29:09 -0400

    setup and connect to mongodb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
+import connectDB from './config/db';
 
-export default function Home() {
+export default async function Home() {
+  await connectDB();
   return (
     <>
       <Hero />
